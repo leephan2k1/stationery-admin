114c32f04f2d7544f9701b02c7e34726b6d15818
leephan2k1 <pttl2k1@gmail.com>
2023-06-08 09:14:42 +0700

    feat: 🎸 add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full',
+  },
+  {
+    path: 'admin',
+    pathMatch: 'full',
     loadComponent: () =>
       import('./views/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'login',
+    pathMatch: 'full',
     loadComponent: () =>
       import('./views/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent,
   },
 ];
 
