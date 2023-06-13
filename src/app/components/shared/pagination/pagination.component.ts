b48e4da668bf32a13ff3f68ac7a4c5508417d2a5
leephan2k1 <pttl2k1@gmail.com>
2023-06-13 10:59:21 +0700

    perf: ⚡️ unsubcribe obserable

--- src/app/components/shared/pagination/pagination.component.ts
+++ src/app/components/shared/pagination/pagination.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   imports: [CommonModule],
@@ -8,16 +10,27 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './pagination.component.html',
   standalone: true,
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnDestroy {
   currentPage: number = 1;
   @Input()
   totalPages: number = 0;
   listNumOfPage: number[] = [];
+  destroy$ = new Subject<void>();
 
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute
   ) {}
+
+  ngOnInit() {
+    this.handleNavigatePage(1);
+    this.activeCurrentPage();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   private handleNavigatePage(n: number) {
     this.router.navigate(['.'], {
@@ -27,28 +40,30 @@ export class PaginationComponent implements OnInit {
   }
 
   private activeCurrentPage() {
-    this.route.queryParams.subscribe((params) => {
-      // @ts-ignore
-      const page = Number(params?.page);
-      if (page !== 0) {
-        this.currentPage = page;
+    this.route.queryParams
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((params) => {
+        // @ts-ignore
+        const page = Number(params?.page);
+        if (page !== 0) {
+          this.currentPage = page;
 
-        let tempArr: number[] = [];
-        if (this.totalPages > 4) {
-          [page - 2, page - 1, page, page + 1].map((e) => {
-            if (e > 0 && e < this.totalPages) {
-              tempArr.push(e);
-            }
-          });
-        } else {
-          tempArr = Array.from(new Array(this.totalPages).keys()).map(
-            (e) => e + 1
-          );
+          let tempArr: number[] = [];
+          if (this.totalPages > 4) {
+            [page - 2, page - 1, page, page + 1].map((e) => {
+              if (e > 0 && e < this.totalPages) {
+                tempArr.push(e);
+              }
+            });
+          } else {
+            tempArr = Array.from(new Array(this.totalPages).keys()).map(
+              (e) => e + 1
+            );
+          }
+
+          this.listNumOfPage = tempArr;
         }
-
-        this.listNumOfPage = tempArr;
-      }
-    });
+      });
   }
 
   onClickPageNumber(page: number) {
@@ -61,11 +76,6 @@ export class PaginationComponent implements OnInit {
     } else if (this.currentPage > 1) {
       this.handleNavigatePage(this.currentPage - 1);
     }
-  }
-
-  ngOnInit() {
-    this.handleNavigatePage(1);
-    this.activeCurrentPage();
   }
 
   trackByFn(index: number, item: any) {
