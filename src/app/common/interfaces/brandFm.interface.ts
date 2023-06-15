f3c0384da82ac89fbcb389a6416a14f06b386cd0
leephan2k1 <pttl2k1@gmail.com>
2023-06-15 09:24:39 +0700

    feat: 🎸 handle add brand

---
+++ src/app/common/interfaces/brandFm.interface.ts
@@ -0,0 +1,5 @@
+import { FormControl } from '@angular/forms';
+
+export interface BrandFm {
+  name: FormControl<string>;
+}
--- src/app/components/partials/brand-edit/brand-edit.component.ts
+++ src/app/components/partials/brand-edit/brand-edit.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { BrandService } from '~/services';
+import { HotToastService } from '@ngneat/hot-toast';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BrandFm } from '~/common/interfaces/brandFm.interface';
 
 @Component({
+  imports: [FormsModule, ReactiveFormsModule],
   selector: 'app-brand-edit',
   templateUrl: './brand-edit.component.html',
   standalone: true,
 })
 export class BrandEditComponent {
+  brandFm: FormGroup<BrandFm>;
+  constructor(
+    private readonly brandService: BrandService,
+    private readonly toast: HotToastService
+  ) {
+    this.brandFm = new FormGroup<BrandFm>({
+      name: new FormControl('', {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+    });
+  }
 
+  submitForm() {
+    const { name } = this.brandFm.value;
+    this.brandService.createBrand({ name: String(name) }).subscribe({
+      next: () => {
+        this.toast.success('Thêm thương hiệu thành công!');
+        this.brandFm.reset();
+      },
+      error: () => {
+        this.toast.error('Thêm thương hiệu thất bại!');
+      },
+    });
+  }
 }
